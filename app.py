3331334ba1587013b78f0dc271c088b7ef5d7b35
danyewest97 <dane20052008@gmail.com>
2024-10-09 14:08:04 -0700

    quick commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, url_for, render_template, request
+import random, string
+
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/")
+def render_home():
+    return render_template('home.html')
+
+if __name__=="__main__":
+    app.run(debug=False)
+
+
+
