2693df44475edf8d543824df5dae15479cd3203d
danyewest97 <dane20052008@gmail.com>
2024-10-10 14:22:30 -0700

    quick commit

--- app.py
+++ app.py
@@ -5,8 +5,20 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_home():
-    # main.print_data()
-    return render_template('home.html', )
+    return render_template('home.html')
+    
+@app.route("/turbines")
+def render_turbines():
+    data = main.get_data("Turbine", "Capacity", "high")
+    return render_template('turbines.html', response=main.turbineToString(data))
+    
+@app.route("/projects")
+def render_projects():
+    return render_template('home.html', response=data)
+    
+@app.route("/locations")
+def render_locations():
+    return render_template('home.html', response=data)
 
 if __name__=="__main__":
     app.run(debug=False)
--- main.py
+++ main.py
@@ -1,7 +1,45 @@
-import json
+import json, sys
 
 def print_data():
     with open("static/data.json") as file:
         data = json.load(file);
     
     print(data)
+
+def get_data(category, key, highlow):
+    with open("static/data.json") as file:
+        data = json.load(file);
+    
+    
+    if (highlow == "high"):
+        highest = -sys.maxsize - 1
+        for element in data:
+            if highest < element[category][key]:
+                result = element
+                highest = element[category][key]
+        return result
+        
+    elif (highlow == "low"):
+        print()
+        
+
+
+def turbineToString(turbine):
+    result = []
+    
+    # Adding the location
+    site = "Site: "
+    site += turbine["Site"]["County"] + ", "
+    site += turbine["Site"]["State"] + ", "
+    site += str(turbine["Site"]["Latitude"]) + " degrees latitude, "
+    site += str(turbine["Site"]["Longitude"]) + " degrees longitude."
+    result.append(str(site))
+    
+    # Adding the turbine data
+    turbine += "Turbine Info: "
+    turbine += str(turbine["Site"]["Latitude"]) + " degrees latitude, "
+    
+    result.append(str(turbine))
+    
+    return result
+    
